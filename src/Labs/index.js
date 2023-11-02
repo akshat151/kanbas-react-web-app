8a7b60ae505a41c8692848b1c2a05b493b088368
Akshat Khare <akshatkhare@Akshats-MacBook-Air.local>
2023-11-02 17:11:21 -0400

    A4 Lab

--- src/Labs/index.js
+++ src/Labs/index.js
@@ -1,9 +1,22 @@
-import Assignment3 from "./a3";
 import Nav from "../nav";
-function Labs() { return (
-<div className="container">
-    <Nav/>
-    <Assignment3/>
-</div> );
+import Assignment3 from "./a3"; 
+import Assignment4 from "./a4"; 
+import { Routes, Route, Navigate } from "react-router";
+import store from "./store";
+import { Provider } from "react-redux";
+
+function Labs() {
+ return (
+  <Provider store={store}>
+    <div className="container">
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Navigate to="a3"/>}/>
+        <Route path="a3" element={<Assignment3/>}/>
+        <Route path="a4" element={<Assignment4/>}/>
+      </Routes>
+    </div>
+   </Provider>
+ );
 }
 export default Labs;
