5de1c50ba39e6a63c96d549f2a1b237b323cd4a7
Akshat Khare <akshat1514@gmail.com>
2023-11-20 22:07:52 -0500

    Final Client 4

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const COURSES_URL = "http://localhost:4000/api/courses";
-const MODULES_URL = "http://localhost:4000/api/modules";
+const COURSES_URL = "https://kanbas-node-server-app-1zfu.onrender.com/api/courses";
+const MODULES_URL = "https://kanbas-node-server-app-1zfu.onrender.com/api/modules";
 
 export const addCourseModule = async (courseId, module) => {
   const response = await axios.post(
